585079f53a4dce41e20196c7be9646a900966920
rubenrtdh <160022630+rubenrtdh@users.noreply.github.com>
2025-03-18 13:25:27 +0100

    Update funciones.py Funciones 1 y 2

--- funciones.py
+++ funciones.py
@@ -17,3 +17,16 @@ def listar_albumes_artista(doc, nombre_artista):
                 resultados.append(info_album)
             return resultados
     return None
+# Ejercicio 2: Contar canciones y álbumes
+def contar_datos(doc):
+    conteo_canciones = {}
+    conteo_generos = {}
+    
+    for artista in doc['artistas']:
+        total_canciones = sum(len(a['canciones']) for a in artista['albumes'])
+        conteo_canciones[artista['nombre']] = total_canciones
+        
+        for album in artista['albumes']:
+            conteo_generos[album['genero']] = conteo_generos.get(album['genero'], 0) + 1
+    
+    return conteo_canciones, conteo_generos
